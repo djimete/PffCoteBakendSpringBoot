e48655835350dd97abb169717362f2845eea1bbb
awa <awa1999fall@gmail.com>
2025-08-21 12:55:19 +0000

    first commit

---
+++ src/main/java/sn/edu/isepdiamniadio/dbe/WorkingExpress/WorkingExpressApplication.java
@@ -0,0 +1,13 @@
+package sn.edu.isepdiamniadio.dbe.WorkingExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/edu/isepdiamniadio/dbe/WorkingExpress/WorkingExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.edu.isepdiamniadio.dbe.WorkingExpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
