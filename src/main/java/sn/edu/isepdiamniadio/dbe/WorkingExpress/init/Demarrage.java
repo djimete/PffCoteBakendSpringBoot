6b9f6c6f56d04856e979615bc38734b4e3f09661
meta djimou <djimou.meta081@gmail.com>
2025-11-24 19:12:41 +0000

    rename du fichier demarrage

--- src/main/java/sn/edu/isepdiamniadio/dbe/WorkingExpress/init/Demarrage.java
+++ src/main/java/sn/edu/isepdiamniadio/dbe/WorkingExpress/init/Demarrage.java
@@ -6,16 +6,16 @@ import org.springframework.stereotype.Component;
 import sn.edu.isepdiamniadio.dbe.WorkingExpress.services.EmailService;
 
 @Component
-public class Demarage implements CommandLineRunner {
+public class Demarrage implements CommandLineRunner {
     private final EmailService emailService;
 
-    public Demarage(EmailService emailService) {
+    public Demarrage(EmailService emailService) {
         this.emailService = emailService;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        emailService.envoyerEmail("djimouansou66@gmail.com","demarage du serveur","bonjour le système vient de démarer");
+        emailService.envoyerEmail("djimouansou66@gmail.com","demarrage du serveur","bonjour le système vient de démarer");
 
     }
 }
